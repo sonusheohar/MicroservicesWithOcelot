ca24fc31adb0f44588a5bc3d2d8537609bf45f8b
Sonu Kumar <sonusheohar15@gmail.com>
2026-05-22 05:59:22 +0530

    Create ocelot.json file and configure into program file

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,8 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 //builder.Services.AddEndpointsApiExplorer();
+
+#region Configure Ocelot dependency 
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+#endregion
 
 var app = builder.Build();
 
@@ -10,5 +17,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// use UseOcelot
+await app.UseOcelot();
 
 app.Run();
